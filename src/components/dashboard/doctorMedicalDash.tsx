5403fc3ca6c21e61d4da4e3060628be404a57f83
Ph00sant <thomascross257@gmail.com>
2023-09-12 15:12:52 -0700

    Additional organization changes

---
+++ src/components/dashboard/doctorMedicalDash.tsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+
+const DoctorMedicalDocuments = () => {
+  const [patients, setPatients] = useState([
+    {
+      id: 1,
+      name: 'John Doe',
+      lastVisit: '2023-08-05',
+      documents: [
+        {
+          id: 1,
+          name: 'Lab Report 1',
+          date: '2023-08-15',
+          type: 'Lab Report',
+          description: 'Results from recent lab tests.',
+        },
+        {
+          id: 2,
+          name: 'Prescription',
+          date: '2023-08-10',
+          type: 'Prescription',
+          description: 'Prescribed medications for the patient.',
+        },
+      ],
+    },
+  ]);
+
+// Will add these as structures to the database later.
+
+  return (
+    <div className="container">
+      <h1 className="my-4">Medical Documents for Doctors</h1>
+      {patients.map((patient) => (
+        <div key={patient.id} className="mb-4">
+          <h3>{patient.name}</h3>
+          <p>Last Visit: {patient.lastVisit}</p>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Document Name</th>
+                <th>Date</th>
+                <th>Type</th>
+                <th>Description</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {patient.documents.map((doc) => (
+                <tr key={doc.id}>
+                  <td>{doc.name}</td>
+                  <td>{doc.date}</td>
+                  <td>{doc.type}</td>
+                  <td>{doc.description}</td>
+                  <td>
+                    <button className="btn btn-primary btn-sm">View</button>
+                    <button className="btn btn-secondary btn-sm">Download</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <button className="btn btn-primary">Add Document</button>
+          <button className="btn btn-info">View All Records</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default DoctorMedicalDocuments;
---
+++ src/components/dashboard/patientMedicalDash.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import DocumentUpload from "../uploadDoc.tsx";
+
+const PatientMedicalDocuments = () => {
+  const [documents, setDocuments] = useState([
+    {
+      id: 1,
+      name: 'Lab Report 1',
+      date: '2023-08-15',
+      type: 'Lab Report',
+      description: 'Results from recent lab tests.',
+    },
+    {
+      id: 2,
+      name: 'Prescription',
+      date: '2023-08-10',
+      type: 'Prescription',
+      description: 'Prescribed medications for your condition.',
+    },
+  ]);
+
+  return (
+    <div className="container">
+      <h1 className="my-4">Medical Documents</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Document Name</th>
+            <th>Date</th>
+            <th>Type</th>
+            <th>Description</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {documents.map((doc) => (
+            <tr key={doc.id}>
+              <td>{doc.name}</td>
+              <td>{doc.date}</td>
+              <td>{doc.type}</td>
+              <td>{doc.description}</td>
+              <td>
+                <button className="btn btn-primary btn-sm">View</button>
+                <button className="btn btn-secondary btn-sm">Download</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <DocumentUpload />
+    </div>
+  );
+};
+
+export default PatientMedicalDocuments;
