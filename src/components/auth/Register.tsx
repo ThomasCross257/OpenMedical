40186a66e2c49062cbf1991428fccfc0bc859f7a
ThomasCross257 <thomascross257@gmail.com>
2023-11-08 17:08:18 -0800

    Added view for Doctor Vs Patient registration

--- src/components/auth/Register.tsx
+++ src/components/auth/Register.tsx
@@ -1,46 +1,30 @@
 import { useState } from 'react';
 import axios from 'axios';
-
-interface RegisterFormData {
-  FirstName: string;
-  LastName: string;
-  DateOfBirth: string;
-  Gender: string;
-  ContactNumber: string;
-  Email: string;
-  Address: string;
-  ZipCode: string;
-  City: string;
-  State: string;
-  Password: string;
-  Appointments: [];
-  MedicalRecords: [];
-  Prescriptions: [];
-  PatientOf: [];
-}
+import { PatientFormData, DoctorFormData } from './models/registrationModels';
 
 function Register() {
-  const [formData, setFormData] = useState<RegisterFormData>({
+  const [registrationType, setRegistrationType] = useState<'doctor' | 'patient'>('patient');
+
+  const [formData, setFormData] = useState<DoctorFormData | PatientFormData>({
     FirstName: '',
     LastName: '',
-    DateOfBirth: '',
-    Gender: '',
     ContactNumber: '',
     Email: '',
+    Password: '',
+    Speciality: '', // Initialize fields specific to doctor registration
+    DateOfBirth: '', // Initialize fields specific to patient registration
+    Gender: '',
     Address: '',
     ZipCode: '',
     City: '',
     State: '',
-    Password: '',
-    Appointments: [],
-    MedicalRecords: [],
-    Prescriptions: [],
-    PatientOf: []
   });
 
-  async function registerPatient(patient: RegisterFormData): Promise<void> {
+  async function registerUser(data: DoctorFormData | PatientFormData): Promise<void> {
     try {
-      const response = await axios.post('https://localhost:7160/api/Patients/createPatient', patient);
+      // Send the data to your API based on the registration type (doctor or patient)
+      const endpoint = registrationType === 'doctor' ? 'createDoctor' : 'createPatient';
+      const response = await axios.post(`https://localhost:7160/api/${endpoint}`, data);
       console.log(response.data);
     } catch (error) {
       console.error(error);
@@ -53,35 +37,21 @@ function Register() {
       ...prevFormData,
       [id]: value,
     }));
-    console.log(formData); // Log the form data to check if it is being updated correctly
-  }
-
-  function generateUserID(fname: string, lname: string): string {
-    const randomNumbers = Math.floor(Math.random() * 900000000) + 100000000;
-    const userID = `${fname.slice(0, 3)}${lname.slice(0, 3)}${randomNumbers}`;
-    return userID;
   }
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    console.log("Form submitted");
     event.preventDefault();
-    // Generate the UserID
-    const generatedUserID = generateUserID(formData.FirstName, formData.LastName);
-
-    // Update the form data with the generated UserID
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      UserID: generatedUserID,
-    }));
-
-    // Now, you can send the updated formData to your API
-    registerPatient(formData);
-    console.log(formData); // Log the form data to check if it is being sent correctly
+    // Call registerUser with the formData
+    registerUser(formData);
   }
 
   return (
     <div className="container">
       <h1>Register</h1>
+      <div>
+        <button onClick={() => setRegistrationType('doctor')}>Doctor Registration</button>
+        <button onClick={() => setRegistrationType('patient')}>Patient Registration</button>
+      </div>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="FirstName" className="form-label">First Name</label>
@@ -99,39 +69,51 @@ function Register() {
           <label htmlFor="Password" className="form-label">Password</label>
           <input type="password" className="form-control" id="Password" onChange={handleInputChange} />
         </div>
-        <div className="mb-3">
-          <label htmlFor="DateOfBirth" className="form-label">Date of Birth</label>
-          <input type="date" className="form-control" id="DateOfBirth" onChange={handleInputChange} />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="Gender" className="form-label">Gender</label>
-          <select className="form-select" aria-label="Default select example" id="Gender" onChange={handleInputChange}>
-            <option value="">Select Gender</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="other">Other</option>
-          </select>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="Address" className="form-label">Address</label>
-          <input type="text" className="form-control" id="Address" onChange={handleInputChange} />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="ZipCode" className="form-label">Zip</label>
-          <input type="text" className="form-control" id="ZipCode" onChange={handleInputChange} />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="City" className="form-label">City</label>
-          <input type="text" className="form-control" id="City" onChange={handleInputChange} />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="State" className="form-label">State</label>
-          <input type="text" className="form-control" id="State" onChange={handleInputChange} />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="ContactNumber" className="form-label">Phone Number</label>
-          <input type="text" className="form-control" id="ContactNumber" onChange={handleInputChange} />
-        </div>
+
+        {/* Render doctor-specific fields if registrationType is 'doctor' */}
+        {registrationType === 'doctor' && (
+          <div>
+            <div className="mb-3">
+              <label htmlFor="Speciality" className="form-label">Speciality</label>
+              <input type="text" className="form-control" id="Speciality" onChange={handleInputChange} />
+            </div>
+          </div>
+        )}
+
+        {/* Render patient-specific fields if registrationType is 'patient' */}
+        {registrationType === 'patient' && (
+          <div>
+            <div className="mb-3">
+              <label htmlFor="DateOfBirth" className="form-label">Date of Birth</label>
+              <input type="date" className="form-control" id="DateOfBirth" onChange={handleInputChange} />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Gender" className="form-label">Gender</label>
+              <select className="form-select" aria-label="Default select example" id="Gender" onChange={handleInputChange}>
+                <option value="">Select Gender</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Address" className="form-label">Address</label>
+              <input type="text" className="form-control" id="Address" onChange={handleInputChange} />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="ZipCode" className="form-label">Zip</label>
+              <input type="text" className="form-control" id="ZipCode" onChange={handleInputChange} />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="City" className="form-label">City</label>
+              <input type="text" className="form-control" id="City" onChange={handleInputChange} />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="State" className="form-label">State</label>
+              <input type="text" className="form-control" id="State" onChange={handleInputChange} />
+            </div>
+          </div>
+        )}
 
         <button type="submit" className="btn btn-success">Submit</button>
       </form>
---
+++ src/components/auth/models/registrationModels.ts
@@ -0,0 +1,24 @@
+interface PatientFormData {
+    FirstName: string;
+    LastName: string;
+    DateOfBirth: string;
+    Gender: string;
+    ContactNumber: string;
+    Email: string;
+    Address: string;
+    ZipCode: string;
+    City: string;
+    State: string;
+    Password: string;
+}
+
+interface DoctorFormData {
+    FirstName: string;
+    LastName: string;
+    Speciality: string;
+    ContactNumber: string;
+    Email: string;
+    Password: string;
+}
+
+export type { DoctorFormData, PatientFormData };
