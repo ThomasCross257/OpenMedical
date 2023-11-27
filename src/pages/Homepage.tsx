73cb2a81a2b7870d0cd520dc000e67a319d1e750
ThomasCross257 <thomascross257@gmail.com>
2023-11-26 20:53:37 -0800

    Images added to the carousel

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,4 +1,8 @@
 import { useState } from 'react';
+import img1 from '../img/pexels-chokniti-khongchum-3938023.jpg';
+import img2 from '../img/pexels-cottonbro-studio-7579831.jpg';
+import img3 from '../img/pexels-photomix-company-1001897.jpg';
+import img4 from '../img/pexels-pixabay-356040.jpg';
 
 function Homepage() {
   const [] = useState(0);
@@ -10,23 +14,35 @@ function Homepage() {
           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
         </div>
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <div style={{ width: 'auto', height: '500px', backgroundColor: 'gray' }}></div>
+            <div style={{ width: 'auto', height: '600px', backgroundColor: '#FFFF0' }}>
+              <img src={img1} className="d-block w-100" alt="..." style={{ objectFit: 'contain', maxWidth: '100%', maxHeight: '100%' }} />
+            </div>
           </div>
           <div className="carousel-item">
-            <div style={{ width: 'auto', height: '500px', backgroundColor: 'blue' }}></div>
+            <div style={{ width: 'auto', height: '600px', backgroundColor: '#FFFF0' }}>
+              <img src={img2} className="d-block w-100" alt="..." style={{ objectFit: 'contain', maxWidth: '100%', maxHeight: '100%' }} />
+            </div>
           </div>
           <div className="carousel-item">
-            <div style={{ width: 'auto', height: '500px', backgroundColor: 'green' }}></div>
+            <div style={{ width: 'auto', height: '600px', backgroundColor: '#FFFF0' }}>
+              <img src={img3} className="d-block w-100" alt="..." style={{ objectFit: 'contain', maxWidth: '100%', maxHeight: '100%' }} />
+            </div>
+          </div>
+          <div className="carousel-item">
+            <div style={{ width: 'auto', height: '600px', backgroundColor: '#FFFF0' }}>
+              <img src={img4} className="d-block w-100" alt="..." style={{ objectFit: 'contain', maxWidth: '100%', maxHeight: '100%' }} />
+            </div>
           </div>
         </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev" style={{ width: '65%', bottom: '0' }}>
           <span className="carousel-control-prev-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Previous</span>
         </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next" style={{ width: '65%', bottom: '0' }}>
           <span className="carousel-control-next-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Next</span>
         </button>
